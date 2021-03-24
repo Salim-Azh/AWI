6870bd432db9c539242a1151682d460affc57e95
Salim_Azh <azharhoussen.salim@gmail.com>
2021-03-24 08:08:11 +0100

    fix body-parser deprecated feat user routes

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 const cors = require("cors")
-
 const path = require("path");
 const app = express()
-const port = process.env.PORT|| 8090
-
+const usersRoutes = require("./routes/users.routes")
 const gamesRoutes = require("./routes/games.routes")
 const editorsRoutes = require("./routes/editor.routes")
 const festivalsRoutes = require("./routes/festivals.routes")
-
 require('dotenv').config({path: './config/.env'})
 require('./config/db')
 
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 //routes
+app.use("/api/users", usersRoutes)
 app.use("/api/games", gamesRoutes)
 app.use("/api/editors", editorsRoutes)
 app.use("/api/festivals", festivalsRoutes)
@@ -25,6 +23,6 @@ app.use("/api/festivals", festivalsRoutes)
 // Serve front
 app.use(express.static(path.join(__dirname, 'front/build')));
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
 })
