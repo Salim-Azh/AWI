d76629958040f74dd33e1ed78535f966bc22321d
Salim_Azh <azharhoussen.salim@gmail.com>
2021-03-24 23:31:49 +0100

    build refactor : cookies module set access_tocken

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/user.model')
 const jwt = require('jsonwebtoken')
+const Cookies = require("cookies")
 
 const exp = '1h'
 const createToken = (id) => {
@@ -25,7 +26,11 @@ module.exports.signIn = async(req,res) => {
     try {
         const user = await UserModel.login(email, pwd)
         const token = createToken(user._id)
-        res.cookie("jwt", token,{httpOnly:true, maxAge: 60*60 /*= 1h */ })
+        new Cookies(req,res).set('access_token',token, {
+            httpOnly: true, //cookie not available through client js code
+            secure: false // true to force https
+        })
+        //res.cookie("jwt", token,{httpOnly:true, maxAge: 60*60 /*= 1h */ })
         res.status(200).json({user:user._id})
     } catch (error) {
         res.status(500).json(error)
