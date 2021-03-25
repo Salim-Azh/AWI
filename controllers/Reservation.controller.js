8adcc0dcd0b1d169fdd55af2f523e09b630169c8
Salim_Azh <azharhoussen.salim@gmail.com>
2021-03-25 11:30:36 +0100

    refactor getReservations

--- controllers/Reservation.controller.js
+++ controllers/Reservation.controller.js
@@ -4,10 +4,34 @@ const EditorModel = require("../models/editors.model")
 
 const mongoose = require("mongoose")
 
+module.exports.getFestivalReservations = async(req, res) => {
+    const response = [{
+        exhibitor: "",
+        reservation: ""
+    }]
+    try {
+        const festival = await FestivalModel.findOne({is_current: true})
+        const reservations = await ReservationsModel.find({festival: festival._id}).select("-games")
+
+        for (let i = 0; i < reservations.length; i++) {
+            const element = reservations[i]
+            const exhibitor = await EditorModel.findById({_id: element.exhibitor}).select("-games") 
+            response[i].exhibitor = exhibitor
+            response[i].reservation = element
+        }
+        res.status(201).json({reservations: response})
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({error})
+    }
+}
+
 // TODO faire correspondre avec le model
 module.exports.addReservation = async(req, res) => {
 
-    const {name, year,
+    const {exhibitor} = req.body
+
+    /*const {name, year,
         nb_tables_premium, nb_tables_standard, nb_tables_low,
         premium_t_price, standard_t_price, low_t_price,
         premium_sm_price, standard_sm_price, low_sm_price} = req.body
@@ -33,7 +57,7 @@ module.exports.addReservation = async(req, res) => {
 
     } catch (error) {
         res.status(400).send({error})
-    }
+    }*/
 }
 
 module.exports.deleteReservation = async(req, res) => {
