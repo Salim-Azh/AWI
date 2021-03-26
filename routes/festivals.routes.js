dc3c4e332e6c5f56eeecd53e99a90ab1843f634d
Salim_Azh <azharhoussen.salim@gmail.com>
2021-03-26 19:48:13 +0100

    feat update reservation

--- routes/festivals.routes.js
+++ routes/festivals.routes.js
@@ -7,15 +7,15 @@ const reservationController = require("../controllers/Reservation.controller")
 
 router.get("/", festivalController.getListOfFestivals)
 router.post("/", festivalController.addFestival)
+router.put("/:id", festivalController.updateFestival)
+router.delete("/:id", festivalController.deleteFestival)
+
+router.put("/:id/current", festivalController.setCurrent)
 
 router.get("/reservations", reservationController.getFestivalReservations)
 router.post("/reservations", reservationController.addReservation)
 
 router.get("/reservations/:id", reservationController.getReservation)
-
-router.put("/:id", festivalController.updateFestival)
-router.put("/:id/current", festivalController.setCurrent)
-
-router.delete("/:id", festivalController.deleteFestival)
+router.put("/reservations/:id", reservationController.updateReservation)
 
 module.exports = router
--- routes/reservation.routes.js
+++ routes/reservation.routes.js
@@ -4,8 +4,6 @@ const router = express.Router()
 // Controllers
 const reservationController = require("../controllers/Reservation.controller")
 
-router.post("/:id", reservationController.updateReservationCheckBox)
 router.delete("/:id", reservationController.deleteReservation)
-router.put("/:id", reservationController.updateReservation)
 
 module.exports = router
