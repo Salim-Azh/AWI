0d5835e5d4156866a84e05c743f03174151c847d
Salim_Azh <azharhoussen.salim@gmail.com>
2021-04-01 16:41:54 +0200

    feat crud zone

--- controllers/zone.controller.js
+++ controllers/zone.controller.js
@@ -1,13 +1,44 @@
-const GameModel = require("../models/games.model")
-const FestivalModel = require("../models/festivals.model")
+const ZoneModel = require("../models/zones.model")
 
 const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports.getZones = async(req,res) => {
     try {
-        const zones = await Zone.find()
+        const zones = await ZoneModel.find()
         res.status(200).json(zones)
     } catch (error) {
         res.status(500).send({error})
+    }
+}
+
+module.exports.addZone = async(req,res) => {
+    const {label, sm_capacity, games} = req.body
+    try {
+        const zone = await ZoneModel.create({label, sm_capacity, games})
+        res.status(201).json(zone)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({error})
+    }
+}
+
+module.exports.updateZone = async(req,res) => {
+    try {
+        ZoneModel.updateOne({_id: req.params.id}, req.body)
+            .then(() => res.status(201).send("success"))
+
+    } catch(error) {
+        res.status(400).send({error})
+    }
+}
+
+
+module.exports.deleteZone = async(req,res) => {
+    try {
+        ZoneModel.deleteOne({_id: req.params.id})
+            .then(() => res.status(201).send("Deleted"))
+    } catch(e) {
+        console.log(e)
+        res.status(400).send({e})
     }
 }
