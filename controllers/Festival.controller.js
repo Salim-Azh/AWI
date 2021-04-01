197c6b0c4608b8d88d1dc6be75cd42496b0d5320
Galois Nicolas <galoisnicolas@gmail.com>
2021-04-01 20:52:18 +0200

    fix: game is undefined

--- controllers/Festival.controller.js
+++ controllers/Festival.controller.js
@@ -94,7 +94,7 @@ module.exports.setCurrent = async(req, res) => {
 }
 
 /**
- * Returns a list of games where each 
+ * Returns a list of games where each
  * game is presented in the current festival
  */
  module.exports.getFestivalGames = async(req, res) => {
@@ -107,12 +107,15 @@ module.exports.setCurrent = async(req, res) => {
         let games=[]
         for (let i = 0; i < reservations.length; i++) {
             const g = reservations[i].games
-            for (let j = 0; j < g.length; j++) { 
-                // g[j] is the game j of the reservation i 
+            for (let j = 0; j < g.length; j++) {
+                // g[j] is the game j of the reservation i
                 const exhibitor = await EditorModel.findById(reservations[i].exhibitor).select("-games")
                 const game = await GameModel.findById(g[j]._id)
                 const state = g[j].state
-                const zone = await ZoneModel.findById(game.zone)
+                let zone
+                if(game) {
+                    zone = await ZoneModel.findById(game.zone)
+                }
                 const editor = await EditorModel.findOne({games: g[j]._id}).select("-games")
                 const proto = g[j].proto
                 games.push({game, state, zone, editor, exhibitor, proto})
@@ -137,7 +140,7 @@ module.exports.getCurrentFestival = async(req, res) => {
 }
 
 module.exports.getFestivalExposantEditor = async(req, res) => {
-   
+
     try {
         const currentFestival = await FestivalModel
             .findOne({is_current: true})
