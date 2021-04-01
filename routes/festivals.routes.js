5d615f31d894ed2652d0ef5e0ef764e5b208dd84
Salim_Azh <azharhoussen.salim@gmail.com>
2021-04-01 09:43:28 +0200

    feat festival routes

--- routes/festivals.routes.js
+++ routes/festivals.routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // Controllers
 const festivalController = require("../controllers/Festival.controller")
 const reservationController = require("../controllers/Reservation.controller")
+const zoneController = require("../controllers/zone.controller")
 
 router.get("/", festivalController.getListOfFestivals)
 router.post("/", festivalController.addFestival)
@@ -21,15 +22,15 @@ router.get("/reservations/:id", reservationController.getReservation)
 router.put("/reservations/:id", reservationController.updateReservation)
 router.delete("/reservations/:id", reservationController.deleteReservation)
 
-/*router.get("/zones", zoneController.getZones)
-router.post("/zones", zoneController.addZone)
+router.get("/zones", zoneController.getZones)
+/*router.post("/zones", zoneController.addZone)
 
 router.get("/zones/:id", zoneController.getZone)
 router.put("/zones/:id", zoneController.updateZone)
 router.delete("/zones/:id", zoneController.deleteZone)*/
 
-
 router.get("/games", festivalController.getFestivalGames)
 
+router.get("/editors", festivalController.getFestivalExposantEditor)
 
 module.exports = router
